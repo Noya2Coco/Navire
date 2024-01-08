87940c01f387430e4b84693b6bfa4b326ecf485a
noah.rousseau <noah.rousseau@CP-SIO-W11-PG03.bts.sio>
2024-01-08 10:25:36 +0100

    commit

--- src/Controller/AisShipTypeController.php
+++ src/Controller/AisShipTypeController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AisShipTypeRepository;
 
-#[Route('/aisshiptype', name: 'aisshiptype')]
+
+#[Route('/aisshiptype', name: 'aisshiptype_')]
 class AisShipTypeController extends AbstractController
 {
     #[Route('/voirtous', name: 'voirtous')]
